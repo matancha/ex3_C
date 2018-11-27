aa9d81a73abba283713b2de85885f1596c163b0e
Matan Toledano <matancha@cs.huji.ac.il>
2018-11-27 19:39:29 +0200

    hmm

--- main.c
+++ main.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
 #include <errno.h>
+#include <string.h>
 #include "stack.h"
 
 int main()
 {
     char infix[100];
+    Stack *stack = stackAlloc(sizeof(int));
 
-    while (fgets(infix, sizeof(infix), stdin) != EOF)
+    while (fgets(infix, sizeof(infix), stdin) != NULL)
     {
         printf("Infix: %s", infix);
 
-        char *endptr;
-        int variable = (int)strtol(str, &endptr, 10);
+        char *symbol = strtok(infix , " ");
+        while (symbol != NULL)
+        {
+            char *endptr;
+            int operand = (int)strtol(symbol, &endptr, 10);
+            push(stack, &operand);
+
+            symbol = strtok(NULL, " ");
+        }
     }
+
+    for(int i = 1; i <= 4; i++)
+    {
+        int headData;
+        pop(stack,&headData);
+        printf("top value is: %d\n",headData);
+    }
+
+    freeStack(&stack);
 }
 
 /*int main()
